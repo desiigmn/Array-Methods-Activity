268ea536c281b8141759768371db4deb060c8d35
Desiree Ann Guzman <desireeann000@gmail.com>
2024-10-19 18:57:16 +0800

    Commit with arrayMethodsActivity.js

--- arrayMethodsActivity.js
+++ arrayMethodsActivity.js
@@ -1,41 +1,44 @@
-// concat()
+// 1. concat()
 arr1 = ["Cecile", "Lone"];
 arr2 = ["Emil", "Tobias", "Linus"];
 let totalArr = arr1.concat(arr2);
 console.log(totalArr.join(", "));
 
-// push()
+// 2. push()
 fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.push("Kiwi");
 console.log(fruits);
 
-// unshift()
+// 3. unshift()
 array1 = [1, 2, 3];
 array1.unshift(4, 5);
 console.log(array1);
 
-// pop()
+// 4. pop()
 fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.pop();
 console.log(fruits);
 
-// shift()
+// 5. shift()
 array2 = [1, 2, 3];
 array2.shift();
 console.log(array2);
 
-// sort()
+// 6. sort()
 fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.sort();
 console.log(fruits);
 
-// slice()
+// 7. slice()
 slicedFruits = ["Banana", "Orange", "Apple", "Mango"];
 slicedFruits.slice(1, 3);
 console.log(slicedFruits);
 
 
-// splice
+// 8. splice
 let monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-monthsArray.splice(0, 2);
+monthsArray.splice(0, 2, "January");
 console.log(monthsArray);
+
+monthsArray.splice(5, 3, "May");  
+console.log(monthsArray); 
